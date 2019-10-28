59119c96f88c6db747ef6a6632bb3138e66dada3
_Deg <degzm@outlook.com>
2019-10-27 19:00:43 -0600

    Ultima version estable
    Se quitaron metodos para ahorrar codigo, y se cambio por uno mas efectivo.

--- app/src/main/java/dev/deg/ipow/MapsActivity.java
+++ app/src/main/java/dev/deg/ipow/MapsActivity.java
@@ -3,20 +3,14 @@ package dev.deg.ipow;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
+import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,12 +21,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
     final Context context = this;
+    Location location;
     double latitud;
     double longitud;
 
@@ -45,64 +38,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
-
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-        }else{locationStart();}
-    }
-
-    private void locationStart() {
-        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Localizacion Local = new Localizacion();
-        Local.setMainActivity(this);
-        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if (!gpsEnabled) {
-            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(settingsIntent);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-            return;
-        }
-        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
-        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
-        Toast.makeText(getApplicationContext(),
-                "Localizacion activada", Toast.LENGTH_SHORT);
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 1000) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                locationStart();
-                return;
-            }
-        }
-    }
-
-    public void setLocation(Location loc) {
-        //Obtener la direccion de la calle a partir de la latitud y la longitud
-        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
-            try {
-                latitud=loc.getLatitude();
-                longitud=loc.getLongitude();
-                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-                List<Address> list = geocoder.getFromLocation(
-                        loc.getLatitude(), loc.getLongitude(), 1);
-                if (!list.isEmpty()) {
-                    /*Address DirCalle = list.get(0);
-                    mensaje2.setText("Mi direccion es: \n"
-                            + DirCalle.getAddressLine(0));*/
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        }else{
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(Objects.requireNonNull(
+                           locationManager.getBestProvider(criteria, false)));
             }
         }
     }
 
     @Override
     public void onMapReady(GoogleMap mapa) {
+        latitud = location.getLatitude();
+        longitud = location.getLongitude();
         LatLng ubi = new LatLng(latitud,longitud);
         mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(ubi,7));
+
         mapa.setMyLocationEnabled(true);
         mapa.getUiSettings().setZoomControlsEnabled( true );
 
@@ -114,11 +69,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 final Dialog dialog = new Dialog(context);
                 dialog.setContentView(R.layout.customdialog);
                 //Text view del titulo, creacion y asignacion
-                TextView tv1 = (TextView) dialog.findViewById(R.id.tv1);
+                TextView tv1 = dialog.findViewById(R.id.tv1);
                 String nombre = marker.getTitle();
                 tv1.setText(nombre);
                 //Text view del snippet (detalles), asignacion y detalles
-                TextView tv2 = (TextView) dialog.findViewById(R.id.tv2);
+                TextView tv2 = dialog.findViewById(R.id.tv2);
                 String snippet = marker.getSnippet();
                 tv2.setText(snippet);
                 //Muestra el dialogo personalizado
