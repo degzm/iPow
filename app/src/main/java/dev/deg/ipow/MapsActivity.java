51a48e90ba1e42bdabb1cf9d4d318e992896f479
_Deg <degzm@outlook.com>
2019-10-28 17:12:44 -0600

    Arreglos menores

--- app/src/main/java/dev/deg/ipow/MapsActivity.java
+++ app/src/main/java/dev/deg/ipow/MapsActivity.java
@@ -25,8 +25,6 @@ import java.util.Objects;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
     final Context context = this;
     Location location;
-    double latitud;
-    double longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,49 +34,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+    }
 
+
+    public void mostrarDialogo(Marker marker){
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.customdialog);
+        //Textview 1
+        TextView tv1 = dialog.findViewById(R.id.tv1);
+        String nombre = marker.getTitle();
+        tv1.setText(nombre);
+        //Text view 2
+        TextView tv2 = dialog.findViewById(R.id.tv2);
+        String snippet = marker.getSnippet();
+        tv2.setText(snippet);
+        dialog.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap mapa) {
+        double latitud = 0;
+        double longitud = 0;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,}, 1000);
+            latitud = location.getLatitude();
+            longitud = location.getLongitude();
+            mapa.setMyLocationEnabled(true);
         }
         else{
             LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
             Criteria criteria = new Criteria();
             if (locationManager != null) {
                 location = locationManager.getLastKnownLocation(Objects.requireNonNull(locationManager.getBestProvider(criteria, false)));
+                latitud = location.getLatitude();
+                longitud = location.getLongitude();
+                mapa.setMyLocationEnabled(true);
             }
         }
-    }
 
-    @Override
-    public void onMapReady(GoogleMap mapa) {
-        latitud = location.getLatitude();
-        longitud = location.getLongitude();
         LatLng ubi = new LatLng(latitud,longitud);
-        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(ubi,14));
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(ubi,18));
 
-        mapa.setMyLocationEnabled(true);
         mapa.getUiSettings().setZoomControlsEnabled( true );
+        mapa.getUiSettings().setMapToolbarEnabled(true);
 
         mapa.addMarker(new MarkerOptions().position(new LatLng(25.4439803, -100.8597785 ))
                 .title("CU Entrada").snippet("Solar"));
 
         mapa.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             public boolean onMarkerClick(final Marker marker) {
-                final Dialog dialog = new Dialog(context);
-                dialog.setContentView(R.layout.customdialog);
-                //Text view del titulo, creacion y asignacion
-                TextView tv1 = dialog.findViewById(R.id.tv1);
-                String nombre = marker.getTitle();
-                tv1.setText(nombre);
-                //Text view del snippet (detalles), asignacion y detalles
-                TextView tv2 = dialog.findViewById(R.id.tv2);
-                String snippet = marker.getSnippet();
-                tv2.setText(snippet);
-                //Muestra el dialogo personalizado
-                dialog.show();
+                mostrarDialogo(marker);
                 return true;
             }
         });
     }
-}
+}//Fin mapsActivity
