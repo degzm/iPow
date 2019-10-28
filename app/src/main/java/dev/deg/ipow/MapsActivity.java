05e5917acfca44f04345edf523b9a9646c8a3898
_Deg <degzm@outlook.com>
2019-10-27 19:02:54 -0600

    Arreglo menor

--- app/src/main/java/dev/deg/ipow/MapsActivity.java
+++ app/src/main/java/dev/deg/ipow/MapsActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
